92433e50747fa2ca18edbc8ff3d25c70e9e3a34f
harsh <harsh.patel@rapidops.com>
2019-06-07 12:20:55 +0530

    second commit

---
+++ exp3.sh
@@ -0,0 +1,18 @@
+#bin/bash
+
+a=$1
+
+b=$2
+
+if [ $a<$b ]
+then
+	echo "$a is max"
+	echo "$b is min"
+else
+	echo "$a is min"
+        echo "$b is max"
+
+if [ $a > 0 AND $b > 0]
+then
+	echo "a&b +"
+
